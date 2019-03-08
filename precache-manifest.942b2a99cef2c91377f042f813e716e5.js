6061c2c8b8223061c5c744a4697dce2a5a537618
Dylan Babbs <dylan.babbs@here.com>
2019-03-07 16:48:56 -0800

    Updates

--- precache-manifest.942b2a99cef2c91377f042f813e716e5.js
+++ precache-manifest.942b2a99cef2c91377f042f813e716e5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/boston-liquor/static/js/runtime~main.9d404ff4.js"
   },
   {
-    "revision": "e64da9d3f971ef34b65b",
-    "url": "/boston-liquor/static/js/main.e64da9d3.chunk.js"
+    "revision": "4fd560187644edc50e1d",
+    "url": "/boston-liquor/static/js/main.4fd56018.chunk.js"
   },
   {
     "revision": "5e39afae62f3dc1b3a98",
     "url": "/boston-liquor/static/js/1.5e39afae.chunk.js"
   },
   {
-    "revision": "e64da9d3f971ef34b65b",
+    "revision": "4fd560187644edc50e1d",
     "url": "/boston-liquor/static/css/main.a4600d35.chunk.css"
   },
   {
-    "revision": "ebf5ce213e3e767305c978e7257a7527",
+    "revision": "61c3a1d67b1f94d4993f3b6b16e376f4",
     "url": "/boston-liquor/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/boston-liquor/precache-manifest.61d81cc35d4ab2fe467b865b0d65a878.js"
+  "/boston-liquor/precache-manifest.942b2a99cef2c91377f042f813e716e5.js"
 );
 
 workbox.clientsClaim();
